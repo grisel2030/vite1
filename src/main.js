eba20691e40f470cd2662d7fb762e3f2995ad8f8
grisel2030 <olivaresgrisel323@gmail.com>
2025-06-17 20:56:11 -0400

    cambios en casa

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 
 document.querySelector('#app').innerHTML = `
-  <div class="bg-[url(./img/fondo2.avif)] w-full h-screen">
+  <div class="bg-[url(./img/fondo2.avif)] h-screen bg-cover bg-no-repeat bg-center">
   
     
   </div>
